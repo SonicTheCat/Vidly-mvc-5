df3888e38e1bfd39719cb8b0decd3e17e4fe4523
sonicthecat <paveliliev90@gmail.com>
2020-02-13 17:26:37 +0200

    Edit action and view added for Movie

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -58,10 +58,47 @@ namespace Vidly.Controllers
         [HttpPost]
         public ActionResult Create(Movie movie)
         {
-            movie.DateAdded = DateTime.Now; 
+            movie.DateAdded = DateTime.Now;
             this.context.Movies.Add(movie);
             this.context.SaveChanges();
 
+            return this.RedirectToAction(nameof(this.Index));
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = this.context
+               .Movies
+               .Include(x => x.Genre)
+               .SingleOrDefault(x => x.Id == id);
+
+            if (movie == null)
+            {
+                return this.HttpNotFound();
+            }
+
+            var viewModel = new EditMovieViewModel
+            {
+                Movie = movie,
+                Genres = this.context.Genres.ToList()
+            };
+
+            return this.View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Movie movie)
+        {
+            var movieFromDb = this.context
+                .Movies
+                .Single(x => x.Id == movie.Id);
+
+            movieFromDb.Name = movie.Name;
+            movieFromDb.ReleaseDate = movie.ReleaseDate;
+            movieFromDb.GenreId = movie.GenreId;
+            movieFromDb.NumberInStock = movie.NumberInStock;
+
+            this.context.SaveChanges();
             return this.RedirectToAction(nameof(this.Index));
         }
     }
