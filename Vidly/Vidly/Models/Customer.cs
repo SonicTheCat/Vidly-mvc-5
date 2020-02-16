87d1d69c33d7bccb1b7efdbd092e01681d893ae8
Pavel Iliev <paveliliev90@gmail.com>
2020-02-16 20:44:21 +0200

    Custom validation attribute introduced!

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -17,6 +17,7 @@ namespace Vidly.Models
         public bool IsSubscribedToNewsletter { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
 
         [Display(Name = "Membership Type")]
--- Vidly/Vidly/Models/MembershipType.cs
+++ Vidly/Vidly/Models/MembershipType.cs
@@ -18,5 +18,8 @@ namespace Vidly.Models
         public byte DurationInMonths { get; set; }
 
         public byte DiscountRate { get; set; }
+
+        public static readonly int Unknown = 0;
+        public static readonly int PayAsYouGo = 1; 
     }
 }
